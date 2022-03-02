4b3abc37383de5b0ab137a250cf57b9a4c7a533e
jakuza <kamil.ch.dev@gmail.com>
2022-03-02 18:37:57 +0100

    Initial RoR application. User and Post models created with some simple association, validation and scopes.

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_many :posts
+
+    validates :name, presence: true
+    validates :name, format: { with: /\A[Aa-zA-Z]+\z/ }
+    scope :created_today, -> { where("created_at > ?", Date.today.beginning_of_day) }
+    scope :with_name, -> (name) { where("name LIKE ?", "#{name}%") }
+end
