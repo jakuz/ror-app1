d9c43f72fd527529da6eb0f6d6ea4d1e930bcaf4
jakuza <kamil.ch.dev@gmail.com>
2022-03-09 13:21:58 +0100

    Added model Comment with associations and validations according to bullets 2-3 of HomeWork section in Lesson3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
 
     validates :name, presence: true
     validates :name, format: { with: /\A[Aa-zA-Z]+\z/ }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_28_193300) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_02_220944) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
@@ -26,5 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_28_193300) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
