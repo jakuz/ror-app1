a575d48755a8a348ac126a3f1883dbbe68c70392
jakuza <kamil.ch.dev@gmail.com>
2022-03-10 23:51:38 +0100

    Bullets 1-6 from HomeWork in Lesson4: created Users, Posts and Comments controllers together with corresponding view files and basic routes configuration.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+    @var1 = "My test variable that will be passed to the index view."
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
