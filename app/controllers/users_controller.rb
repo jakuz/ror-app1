479a69d61acbbf80d85d8ab384b79792ca080df0
jakuza <kamil.ch.dev@gmail.com>
2022-03-11 22:09:00 +0100

    Added basic forms operating on actions in UsersController. Added simple flash notifications in users#destroy action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,52 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "User '#{@user.name}' has been destroyed."
+      redirect_to users_path
+    else
+      flash[:alert] = 'Something wen wrong when deleting user.'
+      redirect_to users_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :comments
 
     validates :name, presence: true
-    validates :name, format: { with: /\A[Aa-zA-Z]+\z/ }
+    validates :name, format: { with: /\A[A-Z][a-z]+ [A-Z][a-z]+(-[A-Z][a-z]+)*\z/ }
     scope :created_today, -> { where("created_at > ?", Date.today.beginning_of_day) }
     scope :with_name, -> (name) { where("name LIKE ?", "#{name}%") }
 end
