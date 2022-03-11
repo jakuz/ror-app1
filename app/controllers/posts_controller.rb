26be227e8a1b32e2439d70bcc664ca06b8e55d1a
jakuza <kamil.ch.dev@gmail.com>
2022-03-11 09:45:18 +0100

    Added example queries in show and index actions of Post controller. Added simple presentation of passed variables in corresponding views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,12 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find_by(id: params[:id])
+    @post ||= "It seems that post of ID=#{params[:id]} does not exist."
   end
 
   def index
+    @posts = Post.all
     @var1 = "My test variable that will be passed to the index view."
   end
 
